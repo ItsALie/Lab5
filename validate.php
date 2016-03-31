1bdcc70cdd8951936f3169b4b79b06dde3bb7810
Haley Booker <haley1652@hotmail.com>
2016-03-31 15:23:04 -0700

    Trying to get php to work

--- validate.php
+++ validate.php
@@ -1,13 +1,21 @@
 <?php
 $users = fopen("users.txt", "r") or die("Unable to open file.");
 while (!feof($users)) {
+    $valid = FALSE;
     $line = fgets($users);
     $line = str_replace("\n", "", $line);
     $word = explode(" ", $line);
-    $first = $word[0];
-    $last = $word[1];
-    $email = $word[2];
-    if ($_POST[first])
+    if ($_POST[first] == $word[0] && $_POST[last] == $word[1] && $_POST[email] == $word[2]) {
+        $valid = TRUE;
+    }
+}
+    if($valid === TRUE) {
+        header("Location:congratulations.html");
+        exit();
+    }
+    else {
+        header("Location:sorry.html");
+        exit();
 }
 fclose($users);
 ?>
